0cad7287575a5a899086ae72fbc58162baf7fc8b
Michael <mvelasquez@csumb.edu>
2018-10-07 06:26:39 +0000

    pic

---
+++ labs/lab4/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Image Carousel</title>
+        <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap."
+        
+        
+    </head>
+    <body>
+        
+        <br/> <br />
+        
+        
+        <br/> <br />
+        
+        
+    </body>
+</html>
